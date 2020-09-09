35196051307f3669fedc49a83e44be300adc0259
yejiang <1140549742@qq.com>
2020-09-10 00:49:58 +0800

    brand的dao层

---
+++ changgou-parrent/changgou-service/changgou-goods-service/src/main/java/com/changgou/goods/dao/BrandMapper.java
@@ -0,0 +1,15 @@
+package com.changgou.goods.dao;
+
+import com.changgou.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+
+/**
+ * Description: <br/>
+ * date: 2020/9/10 0:08<br/>
+ *
+ * @author MuyerJ<br />
+ */
+public interface BrandMapper extends Mapper<Brand> {
+
+}
