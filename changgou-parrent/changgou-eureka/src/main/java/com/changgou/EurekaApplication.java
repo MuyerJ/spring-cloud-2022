976d7b5b948ef8e0cad0c4843ef77e9e3219f4df
yejiang <1140549742@qq.com>
2020-09-09 23:49:54 +0800

    eureka启动问题解决

--- changgou-parrent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
+++ changgou-parrent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.changgou;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -13,6 +14,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(EurekaApplication.class,args);
     }
 }
