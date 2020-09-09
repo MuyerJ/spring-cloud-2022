82c05e81e50eccf746ffe40f3e08c823360c2d11
yejiang <1140549742@qq.com>
2020-09-09 23:37:20 +0800

    创建项目架构

---
+++ changgou-parrent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.changgou;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description: <br/>
+ * date: 2020/9/9 23:25<br/>
+ *
+ * @author MuyerJ<br />
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+
+    }
+}
